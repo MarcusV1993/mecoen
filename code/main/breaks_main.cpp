57aeae6b293fda389c5fe9459f625149b687cfc9
Marcus Vinicius <marcusviniciusgmendes@gmail.com>
2022-03-02 11:59:19 -0300

    Wi-Fi Acess Point added

--- code/main/breaks_main.cpp
+++ code/main/breaks_main.cpp
@@ -16,8 +16,6 @@
 #include "soc/timer_group_struct.h"
 #include "esp_bit_defs.h"
 #include "driver/gptimer.h"
-#include "driver/mcpwm.h"
-#include "soc/mcpwm_periph.h"
 #include "esp_timer.h"
 
 #include "freertos/semphr.h"
@@ -25,6 +23,8 @@
 #include "esp_log.h"
 #include "driver/adc.h"
 #include "esp_adc_cal.h"
+
+#include "wifi_ap.cpp"
 
 
 #define DEFAULT_VREF    1100        //Use adc2_vref_to_gpio() to obtain a better estimate
@@ -38,18 +38,21 @@ static const float signal_to_rms = 1 / (V_SAMPLES * (1000 * SAMPLING_PERIOD_MS))
 #define V0 1110.8 //1110.828
 #define I0 1087.5 //1087.484
 
-// circuit parameters
-#define VCC             5
-// end circuit parameters
+// voltage sensor parameters
+#define ZMPT101B_VCC            4.4
+#define ZMPT101B_R1			  10000
+#define ZMPT101B_R2			  10000
+static const float zmpt101b_dc_bias = (ZMPT101B_VCC / 2) * ZMPT101B_R2 / (ZMPT101B_R1 + ZMPT101B_R2);
+// end voltage sensor parameters
 
 // current sensor parameters
-#define sct013_VCC 				3.3
+#define SCT013_VCC 				3.3
 #define SCT013_NUMBER_TURNS    2000
 #define SCT013_BURDEN_RESISTOR  470
-#define SCT013_R1              22e3
-#define SCT013_R2              10e3
-static const float sct013_dc_bias = VCC * SCT013_R2 / (SCT013_R1 + SCT013_R2);
-static const float sct013_calibration = SCT013_NUMBER_TURNS / sct013_dc_bias;
+#define SCT013_R1             22000
+#define SCT013_R2             10000
+static const float sct013_dc_bias = SCT013_VCC * SCT013_R2 / (SCT013_R1 + SCT013_R2);
+static const float sct013_calibration = SCT013_NUMBER_TURNS / SCT013_BURDEN_RESISTOR;
 // end current sensor parameters
 
 //
@@ -210,61 +213,6 @@ init_adc()
 }
 
 
-static void
-read_voltage(void *arg)
-{
-	Signal *voltage = (Signal *) arg;
-
-	voltage->rms = 0.0;
-	voltage->rms_previous = 0.0;
-
-	int sample = 0;
-
-	while (1)
-	{
-	    voltage->samples[sample] = get_voltage();
-	    voltage->rms += voltage->samples[sample] * voltage->samples[sample];
-		sample++;
-		if (sample >= V_SAMPLES)
-		{
-			sample = 0;
-			voltage->rms_previous = sqrtf(voltage->rms * signal_to_rms);
-			voltage->rms = 0.0;
-		}
-
-		delayMicroseconds(100);
-//		vTaskDelay(1 / portTICK_RATE_MS);
-	}
-}
-
-
-void
-read_current(void *arg)
-{
-	Signal *current = (Signal *) arg;
-	current->rms = 0.0;
-	current->rms_previous = 0.0;
-
-	int sample = 0;
-
-	while (1)
-	{
-		current->samples[sample] = get_adc1_value(channel7);
-//		Test:
-//		current->samples[sample] = sct013_calibration * (get_voltage() - sct013_dc_bias)
-		current->rms += current->samples[sample];
-		sample++;
-		if (sample >= V_SAMPLES)
-		{
-			sample = 0;
-			current->rms_previous = sqrt(current->rms);
-			current->rms = 0.0;
-		}
-//		ojSleepMsec(1.0);
-		vTaskDelay(1 / portTICK_RATE_MS);
-	}
-}
-
 void
 read_phase(void *arg)
 {
@@ -275,11 +223,17 @@ read_phase(void *arg)
 
 	while(1)
 	{
+//		Reading ADC
 		phase->voltage.samples[sample] = get_voltage() - V0;
 		phase->current.samples[sample] = get_adc1_value(channel7) - I0;
 
-//		Convert adc readings to real world value
+//		Convert ADC readings to real world value
+		phase->current.samples[sample] = sct013_calibration * (phase->current.samples[sample] - sct013_dc_bias);
 
+//		Instant power
+		phase->power.samples[sample] = phase->voltage.samples[sample] * phase->current.samples[sample];
+
+//		RMS
 		phase->voltage.rms += phase->voltage.samples[sample]*phase->voltage.samples[sample];
 		phase->current.rms += phase->current.samples[sample]*phase->current.samples[sample];
 
@@ -287,9 +241,11 @@ read_phase(void *arg)
 		if(sample >= V_SAMPLES)
 		{
 			sample = 0;
-//	Imprecision for not calculating rms considering integer multiples of the signal period
+//	Imprecision for not calculating RMS considering integer multiples of the signal period
 			phase->voltage.rms_previous = sqrt(phase->voltage.rms);
 			phase->current.rms_previous = sqrt(phase->current.rms);
+
+			phase->power.rms_previous = phase->voltage.rms_previous * phase->current.rms_previous;
 
 			phase->voltage.rms = 0.0;
 			phase->current.rms = 0.0;
@@ -299,10 +255,8 @@ read_phase(void *arg)
 	}
 }
 
-//Signal voltage;
-//Signal current;
 Circuit_phase phase_a;
-//float vcopy[V_SAMPLES];
+
 static const int REASON = 4;
 float phase_copy[V_SAMPLES/REASON][2];
 
@@ -313,15 +267,20 @@ void app_main()
 {
 	init_adc();
 
-//    printf("\nCreated Signal voltage\n");
-//    xTaskCreatePinnedToCore(read_voltage, "read_voltage", 2048, &voltage, 5, NULL, 1);
-//    printf("\nread_voltage task initialized!\n");
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
+    wifi_init_softap();
+
 
     xTaskCreatePinnedToCore(read_phase, "read_phase", 2048, &phase_a, 5, NULL, 1);
     printf("\nread_phase task initialized!\n");
-
-//    xTaskCreatePinnedToCore(read_current, "read_current", 2048, &current, 5, NULL, 1);
-//    printf("\nread_current task initialized!\n");
 
 	vTaskDelay(500 / portTICK_RATE_MS);
     int cnt = 0;
@@ -335,18 +294,14 @@ void app_main()
 
 		for (int i = 0; i < V_SAMPLES/REASON; i++)
 		{
-//			vcopy[i] = voltage.samples[i];
 			phase_copy[i][0] = phase_a.voltage.samples[i];
 			phase_copy[i][1] = phase_a.current.samples[i];
 		}
 
 		for (int i = 0; i < V_SAMPLES/REASON; i++)
 		{
-//			printf("%.2f\n", vcopy[i]);
-//			printf("V: %.2f\n", voltage.samples[i]);
 			printf("%04.2f %04.2f\n", phase_copy[i][0], phase_copy[i][1]);
 			fflush(stdout);
-//			printf("A: %.2f\n", current.samples[i]);
 			vTaskDelay(10 / portTICK_RATE_MS);
 		}
 
