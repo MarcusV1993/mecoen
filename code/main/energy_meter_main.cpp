4da554dadf87a26475688229a8e05d3bfb0556d3
Marcus Vinicius <marcusviniciusgmendes@gmail.com>
2022-03-08 09:26:35 -0300

    Added web server

--- code/main/energy_meter_main.cpp
+++ code/main/energy_meter_main.cpp
@@ -4,12 +4,17 @@
  * Author: Marcus Vinicius Gonçalves Mendes
  * Advisors: Roseli de Deus Lopes (EPUSP), Marcelo Knörich Zuffo (EPUSP), Alberto Ferreira de Sousa (UFES)
  *
- */
+*/
 
 /*
  * Access point adapted from:
  * https://github.com/espressif/esp-idf/tree/v4.3/examples/wifi/getting_started/softAP
- * */
+*/
+
+/*
+ * Web server adapted from:
+ * https://github.com/caiomb/esp32-http_webserver
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,12 +22,12 @@
 #include <math.h>
 
 #include "esp_timer.h"
-#include "esp_adc_cal.h"
+//#include "esp_adc_cal.h"
 #include "esp_bit_defs.h"
 #include "esp_err.h"
 #include "esp_log.h"
-#include "driver/adc.h"
-#include "driver/gpio.h"
+//#include "driver/adc.h"
+//#include "driver/gpio.h"
 #include "driver/gptimer.h"
 #include "driver/i2c.h"
 #include "freertos/FreeRTOS.h"
@@ -33,64 +38,88 @@
 #include "soc/timer_group_reg.h"
 #include "soc/timer_group_struct.h"
 
+#include "esp_event.h"
 #include "esp_system.h"
 #include "esp_wifi.h"
-#include "esp_event.h"
 #include "nvs_flash.h"
 
 #include "lwip/err.h"
 #include "lwip/sys.h"
+
+#include "definitions.h"
+#include "server.h"
+
+
+//#include "driver/dac.h"
+
+
+//#include <esp_wifi.h>
+//#include <esp_event.h>
+//#include <esp_log.h>
+//#include <esp_system.h>
+//#include <nvs_flash.h>
+//#include <sys/param.h>
+//#include "nvs_flash.h"
+//#include "esp_netif.h"
+//#include "esp_eth.h"
+//#include "protocol_examples_common.h"
+//#include "esp_tls_crypto.h"
+//#include <esp_http_server.h>
+
+
+// http server
+
+// end http server
 
 //#include "wifi_ap.cpp"
 //#include "wifi_sta.cpp"
 
 //#include "ds3231.h"
 
-
 // ADC definitions
-static esp_adc_cal_characteristics_t *adc_chars;
-static const adc_channel_t channel_v = ADC_CHANNEL_6;     // GPIO34
-static const adc_channel_t channel_i = ADC_CHANNEL_7;	  // GPIO35
-static const adc_atten_t atten = ADC_ATTEN_DB_11; // max = 3.9V -> https://docs.espressif.com/projects/esp-idf/en/latest/api-reference/peripherals/adc.html
-static const adc_unit_t unit = ADC_UNIT_1;
-
-#define DEFAULT_VREF    1100        //Use adc2_vref_to_gpio() to obtain a better estimate
-#define NO_OF_SAMPLES   4          //Multisampling
-
-#define V_SAMPLES		2500		// Number of voltage samples
-#define SAMPLING_PERIOD_MS 1
-static const uint32_t SAMPLING_PERIOD_US = 1e6 / V_SAMPLES; // Real sampling frequency slightly lower than 1e6/SAMPLING_PERIOD_US
-static const float signal_to_rms = 1 / (V_SAMPLES * (1000 * SAMPLING_PERIOD_MS));
-
-// voltage sensor parameters
-#define ZMPT101B_VCC            4.4
-#define ZMPT101B_R1			   9840 // 10k
-#define ZMPT101B_R2			   9970 // 10k
-/*\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/*/
-#define ZMPT101B_VMAX		 0.8202 /* Calibrar /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\*/
-/*\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/*/
-static const float zmpt101b_dc_bias = 1192/*(ZMPT101B_VCC * 1000 / 2) * ZMPT101B_R2 / (ZMPT101B_R1 + ZMPT101B_R2)*/; // Calculated: 1.107 V Measured: 1217 mV
-static const float zmpt101b_calibration = (220 * sqrt(2)) / (ZMPT101B_VMAX * 1000 / 2);
-// end voltage sensor parameters
-
-// current sensor parameters
-#define SCT013_VCC 				3.3
-#define SCT013_NUMBER_TURNS    2000
-#define SCT013_BURDEN_RESISTOR  466 // 470
-#define SCT013_R1             21700 // 22k
-#define SCT013_R2              9870 // 10k
-static const float sct013_dc_bias = 1046;/*SCT013_VCC * 1000 * SCT013_R2 / (SCT013_R1 + SCT013_R2);*/ // Calculated: 1,032 V Measured: 1130 mV
-static const float sct013_calibration = (SCT013_NUMBER_TURNS / SCT013_BURDEN_RESISTOR) / 1000;
-// end current sensor parameters
+//static esp_adc_cal_characteristics_t *adc_chars;
+//static const adc_channel_t channel_v = ADC_CHANNEL_6;     // GPIO34
+//static const adc_channel_t channel_i = ADC_CHANNEL_7;	  // GPIO35
+//static const adc_atten_t atten = ADC_ATTEN_DB_11; // max = 3.9V -> https://docs.espressif.com/projects/esp-idf/en/latest/api-reference/peripherals/adc.html
+//static const adc_unit_t unit = ADC_UNIT_1;
+//
+//#define DEFAULT_VREF    1100        //Use adc2_vref_to_gpio() to obtain a better estimate
+//#define NO_OF_SAMPLES   4          //Multisampling
+//
+//#define V_SAMPLES		2500		// Number of voltage samples
+//#define SAMPLING_PERIOD_MS 1
+//static const uint32_t SAMPLING_PERIOD_US = 1e6 / V_SAMPLES; // Real sampling frequency slightly lower than 1e6/SAMPLING_PERIOD_US
+//static const float signal_to_rms = 1 / (V_SAMPLES * (1000 * SAMPLING_PERIOD_MS));
+//
+////// voltage sensor parameters
+//#define ZMPT101B_VCC            4.4
+//#define ZMPT101B_R1			   9840 // 10k
+//#define ZMPT101B_R2			   9970 // 10k
+/////*\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/*/
+//#define ZMPT101B_VMAX		 0.8202 /* Calibrar /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\*/
+/////*\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/*/
+//static const float zmpt101b_dc_bias = 1192/*(ZMPT101B_VCC * 1000 / 2) * ZMPT101B_R2 / (ZMPT101B_R1 + ZMPT101B_R2)*/; // Calculated: 1.107 V Measured: 1217 mV
+//static const float zmpt101b_calibration = (220 * sqrt(2)) / (ZMPT101B_VMAX * 1000 / 2);
+////// end voltage sensor parameters
+//
+////// current sensor parameters
+//#define SCT013_VCC 				3.3
+//#define SCT013_NUMBER_TURNS    2000
+//#define SCT013_BURDEN_RESISTOR  466 // 470
+//#define SCT013_R1             21700 // 22k
+//#define SCT013_R2              9870 // 10k
+//static const float sct013_dc_bias = 1046;/*SCT013_VCC * 1000 * SCT013_R2 / (SCT013_R1 + SCT013_R2);*/ // Calculated: 1,032 V Measured: 1130 mV
+//static const float sct013_calibration = (SCT013_NUMBER_TURNS / SCT013_BURDEN_RESISTOR) / 1000;
+//// end current sensor parameters
 // end ADC definitions
 
 // AP definitions
-#define ESP_WIFI_SSID      "energy_meter"
-#define ESP_WIFI_PASS      "pass1234"
-#define ESP_WIFI_CHANNEL   9
-#define MAX_STA_CONN       3
-
-static const char *TAG_AP = "wifi softAP";
+//#define ESP_WIFI_SSID      "energy_meter"
+//#define ESP_WIFI_PASS      "pass1234"
+//#define ESP_WIFI_CHANNEL   9
+//#define MAX_STA_CONN       3
+//
+//static const char *TAG_AP = "wifi softAP";
 
 // end AP definitions
 
@@ -108,16 +137,16 @@ static const char *TAG_AP = "wifi softAP";
 // end I2C definitions
 
 // structures
-typedef struct Signal
-{
-	float samples[V_SAMPLES];
-	float rms_previous, rms;
-} Signal;
-
-typedef struct Circuit_phase
-{
-	Signal voltage, current, power;
-} Circuit_phase;
+//typedef struct Signal
+//{
+//	float samples[SAMPLING_FREQUENCY];
+//	float rms_previous, rms;
+//} Signal;
+//
+//typedef struct Circuit_phase
+//{
+//	Signal voltage, current, power;
+//} Circuit_phase;
 // end structures
 
 
@@ -166,6 +195,22 @@ ojSleepMsec(double miliseconds)
 }
 // end time related functions
 
+//void
+//dac_signal(void *arg)
+//{
+//	uint8_t value;
+//	int freq = 60;
+//
+//	static int count = 0;
+//	while(1)
+//	{ // 0.15 < value < 2.45 ==> 255 = 3.3; 0.15 = 12; 2.45 = 189 ==> (189 - 12)/2 = 88.5 ==> (189 + 12)/2 = 100.5
+//		value = (uint8_t) ((88.5 * cos(2*3.141592*freq*1e6*esp_timer_get_time())) + 100.5);
+//		//printf("%d\n", value);
+//		count++;
+//		dac_output_voltage(DAC_CHANNEL_1, value);
+//		delayMicroseconds(100);
+//	}
+//}
 
 // ADC related functions
 static void check_efuse(void)
@@ -248,11 +293,12 @@ read_phase(void *arg)
 	{
 //		Reading ADC
 		phase->voltage.samples[sample] = get_adc1_value(channel_v);
+		delayMicroseconds(1);
 		phase->current.samples[sample] = get_adc1_value(channel_i);
 
 //		Convert ADC readings to real world value
-		phase->voltage.samples[sample] = zmpt101b_calibration * (phase->voltage.samples[sample] - zmpt101b_dc_bias);
-		phase->current.samples[sample] =   sct013_calibration * (phase->current.samples[sample] -   sct013_dc_bias);
+//		phase->voltage.samples[sample] = zmpt101b_calibration * (phase->voltage.samples[sample] - zmpt101b_dc_bias);
+//		phase->current.samples[sample] =   sct013_calibration * (phase->current.samples[sample] -   sct013_dc_bias);
 
 //		Instant power
 		phase->power.samples[sample] = phase->voltage.samples[sample] * phase->current.samples[sample];
@@ -262,12 +308,12 @@ read_phase(void *arg)
 		phase->current.rms += phase->current.samples[sample]*phase->current.samples[sample];
 
 		sample++;
-		if(sample >= V_SAMPLES)
+		if(sample >= SAMPLING_FREQUENCY)
 		{
 			sample = 0;
 //	Imprecision for not calculating RMS considering integer multiples of the signal period
-			phase->voltage.rms_previous = sqrt(phase->voltage.rms);
-			phase->current.rms_previous = sqrt(phase->current.rms);
+			phase->voltage.rms_previous = sqrt(phase->voltage.rms / SAMPLING_FREQUENCY);
+			phase->current.rms_previous = sqrt(phase->current.rms / SAMPLING_FREQUENCY);
 
 			phase->power.rms_previous = phase->voltage.rms_previous * phase->current.rms_previous;
 
@@ -279,61 +325,6 @@ read_phase(void *arg)
 	}
 }
 // end ADC related functions
-
-
-// AP related functions
-static void wifi_event_handler(void* arg, esp_event_base_t event_base,
-                                    int32_t event_id, void* event_data)
-{
-    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
-        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
-        ESP_LOGI(TAG_AP, "station " MACSTR " join, AID=%d",
-                 MAC2STR(event->mac), event->aid);
-    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
-        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
-        ESP_LOGI(TAG_AP, "station " MACSTR " leave, AID=%d",
-                 MAC2STR(event->mac), event->aid);
-    }
-}
-
-void wifi_init_softap(void)
-{
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_ap();
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
-                                                        ESP_EVENT_ANY_ID,
-                                                        &wifi_event_handler,
-                                                        NULL,
-                                                        NULL));
-
-    //  convert from C to C++
-    //  https://esp32.com/viewtopic.php?t=1317
-    wifi_config_t wifi_config = { };
-    memset(&wifi_config, 0, sizeof(wifi_config));
-    strcpy(reinterpret_cast<char*>(wifi_config.ap.ssid), ESP_WIFI_SSID);
-    wifi_config.ap.ssid_len = strlen(ESP_WIFI_SSID);
-    wifi_config.ap.channel = ESP_WIFI_CHANNEL;
-    strcpy(reinterpret_cast<char*>(wifi_config.ap.password), ESP_WIFI_PASS);
-    wifi_config.ap.max_connection = MAX_STA_CONN;
-    wifi_config.ap.authmode = WIFI_AUTH_WPA_WPA2_PSK;
-
-    if (strlen(ESP_WIFI_PASS) == 0) {
-        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
-    }
-
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
-
-    ESP_LOGI(TAG_AP, "wifi_init_softap finished. SSID:%s password:%s channel:%d",
-             ESP_WIFI_SSID, ESP_WIFI_PASS, ESP_WIFI_CHANNEL);
-}
-// end AP related functions
 
 
 // NVS related functions
@@ -375,7 +366,7 @@ init_nvs()
 Circuit_phase phase_a;
 
 static const int REASON = 4;
-float phase_copy[V_SAMPLES/REASON][2];
+float phase_copy[SAMPLING_FREQUENCY/REASON][2];
 
 #ifdef __cplusplus
 extern "C"
@@ -388,39 +379,34 @@ void app_main()
 	ret = init_nvs();
     ESP_ERROR_CHECK(ret);
 
-    ESP_LOGI(TAG_AP, "ESP_WIFI_MODE_AP");
-	wifi_init_softap();
-
-//    ret = ds3231_init_desc()
-
-//    ESP_LOGI(TAG_STA, "ESP_WIFI_MODE_STA");
-//    wifi_init_sta();
+    http_server_setup();
 
     xTaskCreatePinnedToCore(read_phase, "read_phase", 2048, &phase_a, 5, NULL, 1);
     printf("\nread_phase task initialized!\n");
 
 	vTaskDelay(500 / portTICK_RATE_MS);
-    int cnt = 0;
+    //int cnt = 0;
 
     printf("\nMain loop initialized!\n");
     while (1)
     {
-		cnt++;
+		//cnt++;
 
 		delayMicroseconds(1000000);
 
-		for (int i = 0; i < V_SAMPLES/REASON; i++)
+		for (int i = 0; i < SAMPLING_FREQUENCY/REASON; i++)
 		{
 			phase_copy[i][0] = phase_a.voltage.samples[i];
 			phase_copy[i][1] = phase_a.current.samples[i];
 		}
 
-		for (int i = 0; i < V_SAMPLES/REASON; i++)
+		for (int i = 0; i < SAMPLING_FREQUENCY/REASON; i++)
 		{
-			printf("%04.2f %04.2f\n", phase_copy[i][0], phase_copy[i][1]);
+			printf("%07.2f %07.2f\n", phase_copy[i][0], phase_copy[i][1]);
 			fflush(stdout);
 			vTaskDelay(10 / portTICK_RATE_MS);
 		}
+		printf("\nVrms = %06.2f; Irms = %06.2f; P = %06.2f\n", phase_a.voltage.rms_previous, phase_a.current.rms_previous, phase_a.power.rms_previous);
 
 		printf("\n\n");
 		fflush(stdout);
---
+++ code/main/server.cpp
@@ -0,0 +1,157 @@
+#include <stdlib.h>
+#include <string.h>
+#include <sys/param.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "driver/gpio.h"
+#include "lwip/sys.h"
+#include "lwip/netdb.h"
+#include "lwip/api.h"
+#include "lwip/err.h"
+#include "server.h"
+
+#include "definitions.h"
+
+#define EXAMPLE_ESP_WIFI_SSID      "mecoen"
+#define EXAMPLE_ESP_WIFI_PASS      "12345678"
+#define EXAMPLE_MAX_STA_CONN       2
+
+static const char *TAG = "wifi softAP";
+static EventGroupHandle_t s_wifi_event_group;
+const static char http_html_hdr[] = "HTTP/1.1 200 OK\r\nContent-type: text/html\r\n\r\n";
+static char http_index_html_server[] = "\
+<html>\
+<body>\
+<center><h1>Projeto de Graduacao em Engenharia Eletrica com Enfase em Eletronica e Sistemas</h1></center>\
+<center><h1>Medidor de Consumo de Energia Eletrica Domestico de Tempo Real com Interface Via Aplicativo Web</h1></center>\
+<center>#                                                                                                       </center>\
+<center>#                                                                                                       </center>\
+</body>\
+</html>";
+/*
+static char http_index_html_server[] = "\
+<html>\
+<body>\
+<center><h1>Projeto de Graduação em Engenharia Elétrica com Ênfase em Eletrônica e Sistemas</h1></center>\
+<center><h1>Medidor de Consumo de Energia Elétrica Doméstico de Tempo Real com Interface Via Aplicativo Web</h1></center>\
+<center>#                                                                                                       </center>\
+<center>#                                                                                                       </center>\
+</body>\
+</html>";
+*/
+
+char message[100];
+
+
+extern Circuit_phase phase_a;
+
+
+static esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+    switch(event->event_id)
+    {
+        case SYSTEM_EVENT_AP_STACONNECTED:
+            ESP_LOGI(TAG, "station:" MACSTR " join, AID=%d",
+                     MAC2STR(event->event_info.sta_connected.mac),
+                     event->event_info.sta_connected.aid);
+            break;
+        case SYSTEM_EVENT_AP_STADISCONNECTED:
+            ESP_LOGI(TAG, "station:" MACSTR "leave, AID=%d",
+                     MAC2STR(event->event_info.sta_disconnected.mac),
+                     event->event_info.sta_disconnected.aid);
+            break;
+        default:
+            break;
+    }
+    return ESP_OK;
+}
+
+void wifi_init_softap()
+{
+    s_wifi_event_group = xEventGroupCreate();
+
+    tcpip_adapter_init();
+    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    //  convert from C to C++
+    //  https://esp32.com/viewtopic.php?t=1317
+    wifi_config_t wifi_config = { };
+    memset(&wifi_config, 0, sizeof(wifi_config));
+    strcpy(reinterpret_cast<char*>(wifi_config.ap.ssid), EXAMPLE_ESP_WIFI_SSID);
+    wifi_config.ap.ssid_len = strlen(EXAMPLE_ESP_WIFI_SSID);
+    strcpy(reinterpret_cast<char*>(wifi_config.ap.password), EXAMPLE_ESP_WIFI_PASS);
+    wifi_config.ap.max_connection = EXAMPLE_MAX_STA_CONN;
+    wifi_config.ap.authmode = WIFI_AUTH_WPA_WPA2_PSK;
+
+    if (strlen(EXAMPLE_ESP_WIFI_PASS) == 0) 
+    {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+    }
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config)); // ESP_IF_WIFI_AP => WIFI_IF_AP
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "wifi_init_softap finished.SSID:%s password:%s",
+             EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+}
+
+
+static void http_server_netconn_serve(struct netconn *conn)
+{
+    struct netbuf *inbuf;
+    err_t err;
+    err = netconn_recv(conn, &inbuf);
+    if (err == ERR_OK) 
+    {
+        char *output = strstr(http_index_html_server, "#");
+//        static int count = 0;
+        sprintf(message, "Vrms = %07.2f", phase_a.voltage.rms_previous); //sprintf(message, "Vrms = %d", count++);
+        strcpy(output + 1, message);
+        output[strlen(message) + 1] = '%';
+        output = strstr(output + 1, "#");
+        sprintf(message, "Vrms = %07.2f", phase_a.current.rms_previous);
+		strcpy(output + 1, message);
+		output[strlen(message) + 1] = '%';
+
+        netconn_write(conn, http_html_hdr, sizeof(http_html_hdr)-1, NETCONN_NOCOPY);
+        netconn_write(conn, http_index_html_server, sizeof(http_index_html_server)-1, NETCONN_NOCOPY);
+    }
+    netconn_close(conn);
+    netbuf_delete(inbuf);
+}
+
+static void http_server(void *pvkeys)
+{
+    struct netconn *conn, *newconn;
+    err_t err;
+    conn = netconn_new(NETCONN_TCP);
+    netconn_bind(conn, NULL, 80);
+    netconn_listen(conn);
+
+    do {
+        err = netconn_accept(conn, &newconn);
+        if (err == ERR_OK) 
+        {
+            http_server_netconn_serve(newconn);
+            netconn_delete(newconn);
+        }
+    } while(err == ERR_OK);
+
+    netconn_close(conn);
+    netconn_delete(conn);
+}
+
+void http_server_setup()
+{
+    wifi_init_softap();
+    xTaskCreate(&http_server, "http_server", 2048, NULL, 5, NULL);
+}
