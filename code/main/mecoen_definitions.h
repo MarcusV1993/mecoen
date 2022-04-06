27c05b282fe0e753b84366808f46fd3c111d856a
Marcus Vinicius <marcusviniciusgmendes@gmail.com>
2022-04-06 07:27:26 -0300

    Merge mecoen_i2c.(cpp + h) to mecoen_main.cpp and mecoen_definitions.h

--- code/main/mecoen_definitions.h
+++ code/main/mecoen_definitions.h
@@ -103,6 +103,21 @@
 #define STORAGE_MAINTAIN 63 // days
 ////// end definitions storage time
 //// end definitions storage
+
+
+//// definitions i2c
+#define I2C_MASTER_SCL_IO           GPIO_NUM_22      /*!< GPIO number used for I2C master clock */
+#define I2C_MASTER_SDA_IO           GPIO_NUM_21      /*!< GPIO number used for I2C master data  */
+#define I2C_MASTER_NUM              0                          /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
+#define I2C_MASTER_FREQ_HZ          400000                     /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_TIMEOUT_MS       1000
+
+
+#define DS3231                 0x68        /*!< Slave address of the DS3231 RTC */
+#define AT24C32                0x57        /*!< Slave address of the AT24C32 EEPROM module embedded in the DS3231 module */
+//// end definitions i2c
 // end definitions
 
 
--- code/main/mecoen_main.cpp
+++ code/main/mecoen_main.cpp
@@ -23,6 +23,7 @@
 #include <time.h>
 //// end includes std c libraries
 
+
 //// includes esp-idf libraries
 ////// includes esp-idf system
 #include "esp_attr.h" // memmory types
@@ -70,6 +71,11 @@
 #include "lwip/sys.h"
 ////// end includes esp-idf wi-fi
 //// end includes esp-idf libraries
+
+
+//// includes esp-lib
+#include "ds3231.h"
+//// end includes esp-lib
 // end includes
 
 //#include "mecoen_adc.h"
@@ -79,7 +85,7 @@
 #include "mecoen_definitions.h"
 #include "mecoen_fft.h"
 //#include "mecoen_time.h"
-#include "mecoen_i2c.h"
+//#include "mecoen_i2c.h"
 //#include "mecoen_storage.h"
 
 
@@ -166,6 +172,11 @@ static int s_retry_num = 0;
 //// global variables storage
 static wl_handle_t s_wl_handle = WL_INVALID_HANDLE; // Handle of the wear levelling library instance
 //// end global variables storage
+
+
+//// global variables i2c
+i2c_dev_t dev;
+//// end global variables i2c
 // end global variables
 
 
@@ -811,6 +822,55 @@ printf("Init end\n");
 //    vEventGroupDelete(s_wifi_event_group);
 }
 //// end functions wi-fi
+
+
+//// functions i2c
+void
+init_rtc_ds3231(struct tm *current_time)
+{
+    ESP_ERROR_CHECK(i2cdev_init());
+
+	memset(&dev, 0, sizeof(i2c_dev_t));
+
+	ESP_ERROR_CHECK(ds3231_init_desc(&dev, 0, I2C_MASTER_SDA_IO, I2C_MASTER_SCL_IO));
+
+    ESP_ERROR_CHECK(ds3231_set_time(&dev, current_time));
+}
+
+void
+rtc_ds3231 (void *arg)
+{
+	float temp;
+	struct tm time;
+
+	while(1)
+	{
+// Check https://www.freertos.org/vtaskdelayuntil.html
+// Try for synchronize ESP32 with RTC
+// To-do: Triple sync ESP32, TRC, NTP
+        vTaskDelay(pdMS_TO_TICKS(7000)); // 7 s
+
+        if (ds3231_get_temp_float(&dev, &temp) != ESP_OK)
+        {
+            printf("Could not get temperature\n");
+            continue;
+        }
+
+        if (ds3231_get_time(&dev, &time) != ESP_OK)
+        {
+            printf("Could not get time\n");
+            continue;
+        }
+
+        /* float is used in printf(). you need non-default configuration in
+         * sdkconfig for ESP8266, which is enabled by default for this
+         * example. see sdkconfig.defaults.esp8266
+         */
+        printf("%04d-%02d-%02d %02d:%02d:%02d, %.2f deg Cel\n", time.tm_year + 1900 /*Add 1900 for better readability*/, time.tm_mon + 1,
+            time.tm_mday, time.tm_hour, time.tm_min, time.tm_sec, temp);
+	}
+}
+//// end functions i2c
 // end functions
 
 
